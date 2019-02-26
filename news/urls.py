2dc094a9023271fe3a7859c266912fc74ec4c56d
moses group <mosesgroup00@gmail.com>
2019-02-26 20:26:33 +0300

    forms, add comment

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path("", PostList.as_view()),
+    path("", PostList.as_view(), name="news"),
     path("category/<slug:slug>/", PostList.as_view(), name="category_post"),
+    path("comment/<int:pk>/", AddComment.as_view(), name="add_comment"),
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.views.generic.base import View
 
 from .models import Post, Category
+from .forms import CommentForm
 
 
 class PostList(View):
@@ -19,4 +20,21 @@ class PostDetail(View):
     """Вывод полной статьи"""
     def get(self, request, pk):
         post = get_object_or_404(Post, id=pk)
-        return render(request, 'news/post-detail.html', {"post": post})
+        form = CommentForm()
+        return render(request, 'news/post-detail.html', {"post": post, "form": form})
+
+
+class AddComment(View):
+    """Добавление комментариев"""
+    def post(self, request, pk):
+        # text = request.POST.get("text")
+        # print(text)
+        form = CommentForm(request.POST)
+        # text = form.changed_data["text"]
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post = Post.objects.get(id=pk)
+            form.save()
+            return redirect("news")
+        else:
+            return HttpResponse(status=400)
